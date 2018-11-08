20883bedf7351ce3aa8587af1726ceeaa714da79
Brad Schrag <brad.schrag@gmail.com>
2018-11-08 19:34:40 +0000

    automatically backed up by learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -22,7 +22,7 @@ def bonus
   epic_tragedy.each do |family, roles|
     roles.each do |role, values|
       if role == :hero || role == :heroine
-        puts "role is #{epic_tragedy[family][roles][role]}"
+        puts "role is #{epic_tragedy[family][roles]}"
         epic_tragedy[family][roles][role][:status] = 'dead'
       end
     end
